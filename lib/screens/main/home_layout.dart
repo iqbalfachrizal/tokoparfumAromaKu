b3499f51820ca4559380c38f9a20b5a92e0f080d
Septian Jati <septianjati820@gmail.com>
2025-12-01 09:37:33 +0700

    Update home_layout.dart

--- lib/screens/main/home_layout.dart
+++ lib/screens/main/home_layout.dart
@@ -14,7 +14,6 @@ class HomeLayout extends StatefulWidget {
 class _HomeLayoutState extends State<HomeLayout> {
   int _selectedIndex = 0;
 
-  // --- DAFTAR HALAMAN ---
   static const List<Widget> _widgetOptions = <Widget>[
     ProductListScreen(), 
     CartScreen(),
@@ -32,19 +31,108 @@ class _HomeLayoutState extends State<HomeLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _widgetOptions.elementAt(_selectedIndex),
-      // ------------------------------------
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Beranda'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Keranjang'),
-          BottomNavigationBarItem(icon: Icon(Icons.feedback), label: 'Kesan'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.purple,
-        unselectedItemColor: Colors.grey,
-        type: BottomNavigationBarType.fixed,
-        onTap: _onItemTapped,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+            colors: [
+              Colors.white,
+              Colors.purple.shade50,
+            ],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              blurRadius: 10,
+              offset: const Offset(0, -5),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  gradient: _selectedIndex == 0
+                      ? LinearGradient(
+                          colors: [Colors.purple.shade300, Colors.deepPurple.shade500],
+                        )
+                      : null,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Icon(
+                  Icons.home,
+                  color: _selectedIndex == 0 ? Colors.white : Colors.grey,
+                ),
+              ),
+              label: 'Beranda',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  gradient: _selectedIndex == 1
+                      ? LinearGradient(
+                          colors: [Colors.purple.shade300, Colors.deepPurple.shade500],
+                        )
+                      : null,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: _selectedIndex == 1 ? Colors.white : Colors.grey,
+                ),
+              ),
+              label: 'Keranjang',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  gradient: _selectedIndex == 2
+                      ? LinearGradient(
+                          colors: [Colors.purple.shade300, Colors.deepPurple.shade500],
+                        )
+                      : null,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Icon(
+                  Icons.feedback,
+                  color: _selectedIndex == 2 ? Colors.white : Colors.grey,
+                ),
+              ),
+              label: 'Kesan',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  gradient: _selectedIndex == 3
+                      ? LinearGradient(
+                          colors: [Colors.purple.shade300, Colors.deepPurple.shade500],
+                        )
+                      : null,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Icon(
+                  Icons.person,
+                  color: _selectedIndex == 3 ? Colors.white : Colors.grey,
+                ),
+              ),
+              label: 'Profil',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
